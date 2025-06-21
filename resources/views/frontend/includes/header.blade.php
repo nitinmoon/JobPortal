b188ff8ab152cc349539056524dfafc65ed341b1
Vikas Sonune <vikas@qubit3tech.io>
2025-06-21 14:51:59 +0530

    minor changes in fonts and menus

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -92,7 +92,7 @@
                             </ul>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">Staffing Solutionsss</a>
+                            <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">Staffing Solutions</a>
                             <ul class="dropdown-menu">
                                 <li class="dropdown-submenu">
                                     <a class="dropdown-item" href="{{route('executive')}}">Executive
