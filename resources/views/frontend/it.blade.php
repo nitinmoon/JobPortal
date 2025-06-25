3005c6cf106a1c5106587e74435b53252dda6a18
Harshal Kothawade <harshalkothawade291@gmail.com>
2025-06-25 15:27:03 +0530

    show images in it and non it page

--- resources/views/frontend/it.blade.php
+++ resources/views/frontend/it.blade.php
@@ -26,87 +26,122 @@
       <div class="container">
         <div class="row text-justify">
           <div class="category">
-            <h2 class="vision-heading ">1. Business Operations</h2>
-            <p>
-              Every product or project requires leadership with vision and focus. As a result, we concentrate on finding highly skilled managers who excel at planning, organizing, and executing effective tech strategies. To do so, we look for experts in time management, problem-solving, and communication, ensuring that each candidate is quality-focused with an in-depth understanding of the latest technology trends.
-            </p>
-            <ul>
-              <li>Project Manager/Coordinator</li>
-              <li>Business Analyst</li>
-              <li>Program Manager</li>
-              <li>Product Manager</li>
-              <li>IT Trainer</li>
-              <li>CRM</li>
-              <li>ERP</li>
-            </ul>
+            <div class="row">
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+              <div class="col-md-6">
+                <h2 class="vision-heading ">1. Business Operations</h2>
+                <p>
+                  Every product or project requires leadership with vision and focus. As a result, we concentrate on finding highly skilled managers who excel at planning, organizing, and executing effective tech strategies. To do so, we look for experts in time management, problem-solving, and communication, ensuring that each candidate is quality-focused with an in-depth understanding of the latest technology trends.
+                </p>
+                <ul>
+                  <li>Project Manager/Coordinator</li>
+                  <li>Business Analyst</li>
+                  <li>Program Manager</li>
+                  <li>Product Manager</li>
+                  <li>IT Trainer</li>
+                  <li>CRM</li>
+                  <li>ERP</li>
+                </ul>
+              </div>
+            </div>
           </div>
 
           <div class="category">
-            <h2 class="vision-heading ">2. Software Development</h2>
-            <p>
-              Whether you are looking to elevate your capabilities or automate processes, finding the right developers can significantly impact the experience for your end user. From enterprise to SaaS solutions, mobile to desktop, our staff connects with unique developers who are up-to-date on the latest coding languages and user experience best practices.
-            </p>
-            <ul>
-              <li>Application Developer</li>
-              <li>Mobile Developer</li>
-              <li>Front End Developer</li>
-              <li>Back End Developer</li>
-              <li>Full Stack Developer</li>
-              <li>Software Engineer</li>
-              <li>Cloud Engineer</li>
-              <li>DevOps</li>
-              <li>Machine Learning</li>
-            </ul>
+            <div class="row">
+              <div class="col-md-6">
+                <h2 class="vision-heading ">2. Software Development</h2>
+                <p>
+                  Whether you are looking to elevate your capabilities or automate processes, finding the right developers can significantly impact the experience for your end user. From enterprise to SaaS solutions, mobile to desktop, our staff connects with unique developers who are up-to-date on the latest coding languages and user experience best practices.
+                </p>
+                <ul>
+                  <li>Application Developer</li>
+                  <li>Mobile Developer</li>
+                  <li>Front End Developer</li>
+                  <li>Back End Developer</li>
+                  <li>Full Stack Developer</li>
+                  <li>Software Engineer</li>
+                  <li>Cloud Engineer</li>
+                  <li>DevOps</li>
+                  <li>Machine Learning</li>
+                </ul>
+              </div>
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+            </div>
           </div>
 
           <div class="category">
-            <h2 class="vision-heading ">3. Quality Assurance</h2>
-            <p>
-              Every great piece of software starts with a clear process and plan. For each phase of the lifecycle to be a success, Quality Assurance is critical. We provide expertise in all facets of QA—enabling our clients to create and deliver high quality products and services that meet the needs and expectations of their customers.
-            </p>
-            <ul>
-              <li>QA Leads & Managers</li>
-              <li>Scrum Master</li>
-              <li>Software Quality Engineers</li>
-              <li>QA Analysts & Testers</li>
-              <li>Database Tester / ETL Engineer</li>
-              <li>SDET</li>
-              <li>Automation Testers</li>
-              <li>Performance Testers</li>
-              <li>UAT Testers</li>
-              <li>OTT/Set-Top Box Testers</li>
-            </ul>
+            <div class="row">
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+              <div class="col-md-6">
+                  <h2 class="vision-heading ">3. Quality Assurance</h2>
+                  <p>
+                    Every great piece of software starts with a clear process and plan. For each phase of the lifecycle to be a success, Quality Assurance is critical. We provide expertise in all facets of QA—enabling our clients to create and deliver high quality products and services that meet the needs and expectations of their customers.
+                  </p>
+                  <ul>
+                    <li>QA Leads & Managers</li>
+                    <li>Scrum Master</li>
+                    <li>Software Quality Engineers</li>
+                    <li>QA Analysts & Testers</li>
+                    <li>Database Tester / ETL Engineer</li>
+                    <li>SDET</li>
+                    <li>Automation Testers</li>
+                    <li>Performance Testers</li>
+                    <li>UAT Testers</li>
+                    <li>OTT/Set-Top Box Testers</li>
+                  </ul>
+              </div>
+            </div>
           </div>
 
           <div class="category">
-            <h2 class="vision-heading ">4. Infrastructure</h2>
-            <p>
-              When you rely on infrastructure and technical support talent to keep your business operational, it is critical that your IT staff are committed to implementing high-quality and up-to-date solutions. As a result, our specialized recruiters look for professionals who understand how to streamline operations and reduce costs—all while monitoring and managing the tech you need for day-to-day operations. Whether you are looking to implement a new system or hire in-house staff, our individualized approach can help you find the necessary technical support.
-            </p>
-            <ul>
-              <li>Cybersecurity</li>
-              <li>Cloud Infrastructure & DevOps</li>
-              <li>Security Engineer/Architect</li>
-              <li>Systems/Network Engi neer</li>
-              <li>Systems/Network Administration</li>
-              <li>Network Operations Center</li>
-              <li>Performance Testers</li>
-              <li>Desktop Support</li>
-              <li>Help Desk</li>
-            </ul>
+            <div class="row">
+              <div class="col-md-6">
+                <h2 class="vision-heading ">4. Infrastructure</h2>
+                <p>
+                  When you rely on infrastructure and technical support talent to keep your business operational, it is critical that your IT staff are committed to implementing high-quality and up-to-date solutions. As a result, our specialized recruiters look for professionals who understand how to streamline operations and reduce costs—all while monitoring and managing the tech you need for day-to-day operations. Whether you are looking to implement a new system or hire in-house staff, our individualized approach can help you find the necessary technical support.
+                </p>
+                <ul>
+                  <li>Cybersecurity</li>
+                  <li>Cloud Infrastructure & DevOps</li>
+                  <li>Security Engineer/Architect</li>
+                  <li>Systems/Network Engi neer</li>
+                  <li>Systems/Network Administration</li>
+                  <li>Network Operations Center</li>
+                  <li>Performance Testers</li>
+                  <li>Desktop Support</li>
+                  <li>Help Desk</li>
+                </ul>
+              </div>
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+            </div>
           </div>
 
           <div class="category">
-            <h2 class="vision-heading ">5. Data Management</h2>
-            <ul>
-              <li>Business Intelligence</li>
-              <li>Big Data</li>
-              <li>Database Developer</li>
-              <li>Data Warehouse</li>
-              <li>Data Analyst</li>
-              <li>Data Science</li>
-              <li>Data Extraction & ETL</li>
-            </ul>
+            <div class="row">
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+              <div class="col-md-6">
+                <h2 class="vision-heading ">5. Data Management</h2>
+                <ul>
+                  <li>Business Intelligence</li>
+                  <li>Big Data</li>
+                  <li>Database Developer</li>
+                  <li>Data Warehouse</li>
+                  <li>Data Analyst</li>
+                  <li>Data Science</li>
+                  <li>Data Extraction & ETL</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- resources/views/frontend/noniit.blade.php
+++ resources/views/frontend/noniit.blade.php
@@ -26,100 +26,135 @@
       <div class="container">
         <div class="row text-justify">
           <div class="category">
-            <h2 class="vision-heading ">1. Manufacturing</h2>
-            <p>
-              Manufacturing forms the backbone of our economic system. The goods rolling off production lines nationwide helps keeping the economy humming while providing the products consumers and businesses need to live and work comfortably. Appliances, heating and cooling units, lawn and garden equipment, lighting – the items we buy for home and work come from manufacturers large and small.
-            </p>
-            <p>The vast variety of products manufactured throughout the country demand an enormous range of fasteners, MRO, and safety supplies to put them together. From standard fasteners and customized parts, to cleaning supplies and tools, manufacturers need to know they’ll have the parts they need to keep producing their products and meet customer demand.</p>
-
+            <div class="row">
+              <div class="col-md-6">
+                <h2 class="vision-heading ">1. Manufacturing</h2>
+                <p>
+                  Manufacturing forms the backbone of our economic system. The goods rolling off production lines nationwide helps keeping the economy humming while providing the products consumers and businesses need to live and work comfortably. Appliances, heating and cooling units, lawn and garden equipment, lighting – the items we buy for home and work come from manufacturers large and small.
+                </p>
+                <p>The vast variety of products manufactured throughout the country demand an enormous range of fasteners, MRO, and safety supplies to put them together. From standard fasteners and customized parts, to cleaning supplies and tools, manufacturers need to know they’ll have the parts they need to keep producing their products and meet customer demand.</p>
+              </div>
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+            </div>
           </div>
 
           <div class="category">
-            <h2 class="vision-heading ">2.Automotive</h2>
-            <p>
-              <span style="font-weight: bold;"> Staffing Services Staffing</span> Solutions works closely with a range of international automotive clients and tiered suppliers, helping them to source the skilled and experienced staff and/or contractors for all of their design, process and automotive engineering roles. We are committed to delivering consistent and cost-effective staffing solutions on every occasion, and have established a strong market presence.
-            </p>
-            <p>Our automotive recruitment teams have good experience in sourcing high-quality personnel within the core functions of design, quality, project management, engineering, operations and maintenance. Automotive include the following disciplines.</p>
-            <ul>
-              <li>Design</li>
-              <li>Quality / Supplier Quality</li>
-              <li>Lean / Continuous Improvement</li>
-              <li>Procurement</li>
-              <li>Manufacturing</li>
-              <li>Maintenance</li>
-
-            </ul>
+            <div class="row">
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+              <div class="col-md-6">
+                <h2 class="vision-heading ">2.Automotive</h2>
+                <p>
+                  <span style="font-weight: bold;"> Staffing Services Staffing</span> Solutions works closely with a range of international automotive clients and tiered suppliers, helping them to source the skilled and experienced staff and/or contractors for all of their design, process and automotive engineering roles. We are committed to delivering consistent and cost-effective staffing solutions on every occasion, and have established a strong market presence.
+                </p>
+                <p>Our automotive recruitment teams have good experience in sourcing high-quality personnel within the core functions of design, quality, project management, engineering, operations and maintenance. Automotive include the following disciplines.</p>
+                <ul>
+                  <li>Design</li>
+                  <li>Quality / Supplier Quality</li>
+                  <li>Lean / Continuous Improvement</li>
+                  <li>Procurement</li>
+                  <li>Manufacturing</li>
+                  <li>Maintenance</li>
+                </ul>
+              </div>
+            </div>
           </div>
 
           <div class="category">
-            <h2 class="vision-heading ">3.FMCG</h2>
-            <p>Now in the changing scenario, store visits by consumers are shrinking. As more consumer products companies have a direct relationship with the purchasers of their products, even as robust data analytics allow personalization and localization, leading to ever more differentiated shopper segments, and a changing job profiles.
-            </p>
-            <p>O&G Skills has a combination of Subject Matter Experts and Sourcing/Talent Mapping Experts and Contract Staffing Experts in the FMCG/Retail sector, which is good for any kind conceivable hiring solution required by any client in this industry. Whether it is a long-standing skills/technology or new upcoming one, our experts are completely clued on and up to date and ready to close your position.</p>
-            <ul>
-              <li>Area Sales Manager</li>
-              <li>AVP Marketing Communications</li>
-              <li>General Manager Sales</li>
-              <li>Brand Manager (Nutrition)</li>
-              <li>Brand Manager (Fashion)</li>
-              <li>Sales Manager (Lighting)</li>
-              <li>Head Regional Operations</li>
-              <li>Senior Designer – Apparels</li>
-              <li>Head of Modern Trade</li>
-              <li>Category Manager</li>
-              <li>Brand Manager (FMCG)</li>
-            </ul>
+            <div class="row">
+              <div class="col-md-6">
+                <h2 class="vision-heading ">3.FMCG</h2>
+                <p>Now in the changing scenario, store visits by consumers are shrinking. As more consumer products companies have a direct relationship with the purchasers of their products, even as robust data analytics allow personalization and localization, leading to ever more differentiated shopper segments, and a changing job profiles.
+                </p>
+                <p>O&G Skills has a combination of Subject Matter Experts and Sourcing/Talent Mapping Experts and Contract Staffing Experts in the FMCG/Retail sector, which is good for any kind conceivable hiring solution required by any client in this industry. Whether it is a long-standing skills/technology or new upcoming one, our experts are completely clued on and up to date and ready to close your position.</p>
+                <ul>
+                  <li>Area Sales Manager</li>
+                  <li>AVP Marketing Communications</li>
+                  <li>General Manager Sales</li>
+                  <li>Brand Manager (Nutrition)</li>
+                  <li>Brand Manager (Fashion)</li>
+                  <li>Sales Manager (Lighting)</li>
+                  <li>Head Regional Operations</li>
+                  <li>Senior Designer – Apparels</li>
+                  <li>Head of Modern Trade</li>
+                  <li>Category Manager</li>
+                  <li>Brand Manager (FMCG)</li>
+                </ul>
+              </div>
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+            </div>
           </div>
 
           <div class="category">
-            <h2 class="vision-heading ">4. Healthcare</h2>
-            <p><span style="font-weight: bold;">Liftale Staffing Services </span>
-              is catering in India and within multiple industries. Within Healthcare, Liftale Staffing Services, Health has been attracting and retaining healthcare professionals. Liftale Staffing Services has proven time and time again to develop strong, long-term and mutually beneficial relationships across all sub-sectors of healthcare. We have the expertise to identify critical talent within a range of healthcare environments.
-
-            </p>
-            <p>Through our Core Staffing capability, we can connect you with quality and dependable healthcare professionals. Liftale Staffing Services Health Recruiters work to provide support across the healthcare industry and deliver highly qualified staffing services to your team.</p>
-            <ul>
-              <li>Allied & Healthcare Professionals</li>
-              <li>Pharma & Biotech</li>
-              <li>Revenue Cycle Management</li>
-              <li>Pharmacy</li>
-              <li>Member Support Services</li>
-              <li>Administrative & Clerical</li>
-              <li>Operations</li>
-
-            </ul>
+            <div class="row">
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+              <div class="col-md-6">
+                <h2 class="vision-heading ">4. Healthcare</h2>
+                <p><span style="font-weight: bold;">Liftale Staffing Services </span>
+                  is catering in India and within multiple industries. Within Healthcare, Liftale Staffing Services, Health has been attracting and retaining healthcare professionals. Liftale Staffing Services has proven time and time again to develop strong, long-term and mutually beneficial relationships across all sub-sectors of healthcare. We have the expertise to identify critical talent within a range of healthcare environments.
+                </p>
+                <p>Through our Core Staffing capability, we can connect you with quality and dependable healthcare professionals. Liftale Staffing Services Health Recruiters work to provide support across the healthcare industry and deliver highly qualified staffing services to your team.</p>
+                <ul>
+                  <li>Allied & Healthcare Professionals</li>
+                  <li>Pharma & Biotech</li>
+                  <li>Revenue Cycle Management</li>
+                  <li>Pharmacy</li>
+                  <li>Member Support Services</li>
+                  <li>Administrative & Clerical</li>
+                  <li>Operations</li>
+                </ul>
+              </div>
+            </div>
           </div>
           <div class="category">
-            <h2 class="vision-heading ">5. Engineering</h2>
-            <p>The right asset can determine the success of your critical projects and programs.<span style="font-weight: bold;">Liftale Staffing Services</span> has the expertise to match the right engineers with your organization in the shortest amount of time possible.</p>
-            <ul>
-              <li>Mechanical engineer</li>
-              <li>Civil engineer</li>
-              <li>Designer/drafter</li>
-              <li>Electrical engineer</li>
-              <li>Electrical/mechanical assembler</li>
-              <li>Electrical technician</li>
-
-            </ul>
+            <div class="row">
+              <div class="col-md-6">
+                <h2 class="vision-heading ">5. Engineering</h2>
+                <p>The right asset can determine the success of your critical projects and programs.<span style="font-weight: bold;">Liftale Staffing Services</span> has the expertise to match the right engineers with your organization in the shortest amount of time possible.</p>
+                <ul>
+                  <li>Mechanical engineer</li>
+                  <li>Civil engineer</li>
+                  <li>Designer/drafter</li>
+                  <li>Electrical engineer</li>
+                  <li>Electrical/mechanical assembler</li>
+                  <li>Electrical technician</li>
+                </ul>
+              </div>
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+            </div>
           </div>
           <div class="category">
-            <h2 class="vision-heading ">6.Chemical</h2>
-            <p><span style="font-weight: bold;"> Liftale Staffing Services</span> workforce solutions experts have transformed the talent acquisition and mobility strategies of companies in the energy, process and infrastructure sectors and have good experience in the chemicals industry.
-
-            </p>
-            <p>
-              Our chemicals recruitment specialists are passionate about improving the lives of workers in the chemicals sector and companies in need of the absolute best talent for their projects.
-            </p>
-            <p>Our talented recruitment team will work alongside you to create an effective talent acquisition and pipeline strategy to ensure you will always have access to the best chemicals Engineers and permanent employees available.</p>
-            <ul>
-              <li>Sales</li>
-              <li>Marketing</li>
-              <li>Manufacturing</li>
-              <li>Research & Development</li>
-              <li>Administration</li>
-              <li>Business Management</li>
-
-            </ul>
+            <div class="row">
+              <div class="col-md-6 d-flex align-items-center">
+                <img src="public/frontend/assets/images/aboutus-page.jpg" alt="Business Meeting">
+              </div>
+              <div class="col-md-6">
+                <h2 class="vision-heading ">6.Chemical</h2>
+                <p><span style="font-weight: bold;"> Liftale Staffing Services</span> workforce solutions experts have transformed the talent acquisition and mobility strategies of companies in the energy, process and infrastructure sectors and have good experience in the chemicals industry.
+                </p>
+                <p>
+                  Our chemicals recruitment specialists are passionate about improving the lives of workers in the chemicals sector and companies in need of the absolute best talent for their projects.
+                </p>
+                <p>Our talented recruitment team will work alongside you to create an effective talent acquisition and pipeline strategy to ensure you will always have access to the best chemicals Engineers and permanent employees available.</p>
+                <ul>
+                  <li>Sales</li>
+                  <li>Marketing</li>
+                  <li>Manufacturing</li>
+                  <li>Research & Development</li>
+                  <li>Administration</li>
+                  <li>Business Management</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
